ed188e565810f90dd21fb0665b642cddd2d011ac
Narazaka <info@narazaka.net>
2016-05-11 14:08:40 +0900

    config options

--- src/bin/xlsx2seed.js
+++ src/bin/xlsx2seed.js
@@ -1,7 +1,10 @@
+const fs = require('fs');
 const path = require('path');
 const jsyaml = require('js-yaml');
 const Xlsx2Seed = require('../lib/xlsx2seed').Xlsx2Seed;
 const commander = require('commander');
+
+const default_config_file = 'xlsx2seed.yml';
 
 const program = commander
   .version(require('../package.json').version)
@@ -12,6 +15,8 @@ const program = commander
   .option('-i, --input [path]', 'input directory', String, '.')
   .option('-o, --output [path]', 'output directory', String, '.')
   .option('-d, --stdout', 'output one sheets to stdout')
+  .option('-c, --config [path]', 'config file (default: xlsx2seed.yml)', String, '')
+  .option('-C, --config-content [yaml string]', 'config content', String, '')
   .on('--help', () => {
     console.log('  Examples:');
     console.log('');
@@ -24,11 +29,35 @@ const program = commander
     console.log('    # foo subdivide prefix 1 / bar subdivide postfix 2 / baz subdivide prefix 1 and postfix 2');
     console.log('    $ xlsx2seed hoge.xlsx huga.xlsx -S 2:foo,bar:2,1:baz:2');
     console.log('');
+    console.log('    # column names row is 3 (2 in zero origin)');
+    console.log('    $ xlsx2seed hoge.xlsx huga.xlsx -C "column_names_row: 2"');
+    console.log('');
   })
   .parse(process.argv);
 
 const files = program.args;
 if (!files.length) program.help();
+
+function get_config(program) {
+  try {
+    if (program.configContent) {
+      return jsyaml.load(program.configContent);
+    } else {
+      if (program.config) {
+        return jsyaml.load(fs.readFileSync(program.config, {encoding: 'utf8'}));
+      } else if (fs.existsSync(default_config_file)) {
+        return jsyaml.load(fs.readFileSync(default_config_file, {encoding: 'utf8'}));
+      } else {
+        return;
+      }
+    }
+  } catch(error) {
+    console.error("load config failed!");
+    console.error(error.toString());
+    process.exit(1);
+  }
+}
+const config = get_config(program);
 
 function sheet_name_subdivide_rule(sheet_name) {
   const result = sheet_name.match(/^(?:(\d+):)?(.+?)(?::(\d+))?$/);
@@ -87,7 +116,7 @@ for (const file of files) {
       _console.log(`      ignore: skip`);
       continue;
     }
-    const sheet = xlsx2seed.sheet(sheet_name);
+    const sheet = xlsx2seed.sheet(sheet_name, config);
     if (!sheet.has_id_column()) {
       _console.log(`      warning: id column not found -> skip!`);
       continue;
